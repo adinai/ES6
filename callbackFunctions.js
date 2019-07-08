8f41bea0bfb5b6702ce8595ed06cf42845767ee2
Adina Kaiymova <adina.kaiymova@gmail.com>
2019-07-08 18:44:11 +0900

    updated callback function

--- callbackFunctions.js
+++ callbackFunctions.js
@@ -33,3 +33,37 @@ const getLesserThanFive = (num => num < 5)
 const lesserThanFive = numbers.filter(getLesserThanFive)
 /* In this case, getLesserThanFive is callback function.
 Array.filter is a function accepts a callback function. */
+
+/*------------ How to write callback functions----------- */
+
+//Create a function that accepts another function as an argument
+const callbackAcceptingFunction =(fn) => {
+    //Calls the function with any required arguments
+    return fn(1, 2, 3)
+}
+//Callback gets arguments from the above call
+const callback = (arg1, arg2, arg3) => {
+    return arg1 + arg2 + arg3
+}
+
+//Passing a callback into a callback accepting function
+const result = callbackAcceptingFunction(callback)
+console.log(result) //6
+
+
+
+/* Synchronous Function Callback */
+
+/* If your code executes in a top to bottom,
+ left to right fashion, sequentially, waiting 
+ until one code has finished before the next line begins,
+your code is synchronous. Array.filter example above */
+
+/* Asynchronous here means that, if Javascript needs to wait
+for something to complete, it will execute the rest of the
+tasks given to it while waiting. An example of an asynchronous
+function is setTimeout.*/
+
+setTimeout(callback, 1000)
+
+/* Let's see 
